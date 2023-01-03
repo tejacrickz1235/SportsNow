4ff181a3748703971162bf813ba30959d954c6b4
tejacrickz1235 <glaxmisainath6@gmail.com>
2023-01-04 00:12:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
   const path = window.location.pathname; 
   console.log(path)
   return (
-    <BrowserRouter > 
+    <BrowserRouter basename='/SportsNow'> 
         
         
       <Routes>
